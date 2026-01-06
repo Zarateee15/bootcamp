d873f2391ac8ce24c6911dd082ac9ef881df16b1
Jorge Zárate <ezeg754@gmail.com>
2026-01-06 09:51:02 -0300

    Cambio de funcionalidad

--- ejercicios_dia1/src/ejercicio_7.java
+++ ejercicios_dia1/src/ejercicio_7.java
@@ -2,16 +2,10 @@
 
 public class ejercicio_7 {
     public static void main(String[] args) {
-        System.out.print("Divisibles entre 2: ");
+        System.out.print("Divisibles entre 2 y 3: ");
         for (int i = 1; i < 101; i++) {
-            if (i%2 == 0){
+            if (i%2 == 0 && i%3==0){
                 System.out.print(" "+i+" ");
-            };
-        }
-        System.out.print("\nDivisibles entre 3: ");
-        for (int j = 1; j < 101; j++) {
-            if (j%3 == 0){
-                System.out.print(" "+j+" ");
             };
         }
     }
